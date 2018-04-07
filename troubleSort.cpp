90d68cbe443a25cb84492b78138af2f9e5b36e8f
Omar M. Adel <eng.omaradel95@yahoo.com>
2018-04-07 19:13:31 +0200

    There was a mistake when the list is even

--- troubleSort.cpp
+++ troubleSort.cpp
@@ -5,35 +5,36 @@
 
 using namespace std;
 
-void troubleSort(vector<unsigned int>& v, int size)
+void troubleSort(vector<long long>::iterator it1, vector<long long>::iterator it2)
 {
-	for (auto itOuter = 0; itOuter < size-2; itOuter++)
+	int even = 1, odd = 2;
+	if ((it2 - it1) % 2 == 0)
+		swap(even, odd);
+	for (auto itOuter = it1; itOuter != it2 - 2; itOuter++)
 	{
-		for (auto itEven = 0; itEven < size-1; itEven +=2)
+		for (auto itEven = it1; itEven != it2 - even; itEven += 2)
 		{
-			if (v[itEven] > v[itEven +2])
-				swap(v[itEven], v[itEven +2]);
+			if ((*itEven) > (*(itEven + 2)))
+				swap(*itEven, *(itEven + 2));
 		}
-		for (auto itOdd = 1; itOdd < size - 2 ; itOdd += 2)
+		for (auto itOdd = it1 + 1; itOdd != it2 - odd; itOdd += 2)
 		{
-			if (v[itOdd] > v[itOdd + 2])
-				swap(v[itOdd], v[itOdd + 2]);
+			if ((*itOdd) > (*(itOdd + 2)))
+				swap(*itOdd, *(itOdd + 2));
 		}
 	}
 }
 
-int compare(vector<unsigned int> v1, vector<unsigned int> v2)
+int compare(vector<long long> v1, vector<long long> v2)
 {
-	int i,flag=1;
+	if (v1 == v2)
+		return -1;
+	int i;
 	for (i = 0; i < v1.size(); i++)
 	{
 		if (v1[i] > v2[i])
-		{
 			break;
-			flag = 0;
-		}
 	}
-	if (flag == 1) return -1;
 	return i;
 }
 
@@ -41,16 +42,16 @@ int main()
 {
 	int t;
 	cin >> t;
-	
+
 	for (int i = 1; i <= t; i++)
 	{
-		unsigned int numberOfValues;
+		int numberOfValues;
 		cin >> numberOfValues;
-		vector<unsigned int>values(numberOfValues);
+		vector<long long>values(numberOfValues);
 		for (int i = 0; i < numberOfValues; i++)
 			cin >> values[i];
-		vector<unsigned int> troubleValues = values;
-		troubleSort(troubleValues, numberOfValues);
+		vector<long long> troubleValues = values;
+		troubleSort(troubleValues.begin(), troubleValues.end());
 		sort(values.begin(), values.end());
 		int index = compare(troubleValues, values);
 		if (index == -1)
